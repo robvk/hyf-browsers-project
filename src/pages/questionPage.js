464af2797124738bca41ddc22fd823604e358c14
Hikmet Dag <temkihgad@gmail.com>
2022-08-28 22:00:37 +0200

    adding nextquestion button and send answer to data quiz

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -23,16 +23,20 @@ export const initQuestionPage = () => {
 
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
+    answerElement.addEventListener('click', function (){
+      currentQuestion.selected = key;
+      answerElement.classList.add('Chosen'); 
+     setTimeout(nextQuestion,1000)
+    });
     answersListElement.appendChild(answerElement);
   }
 
-  document
-    .getElementById(NEXT_QUESTION_BUTTON_ID)
-    .addEventListener('click', nextQuestion);
+  
 };
 
 const nextQuestion = () => {
-  quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+quizData.currentQuestionIndex ++
 
-  initQuestionPage();
+initQuestionPage();
+
 };
