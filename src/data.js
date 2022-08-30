9f358f33afbd1ddc2efc6b3a8232ae61a967de8d
red <rediettesfaye232@gmail.com>
2022-08-30 12:09:57 +0200

    redietbrowserproject

--- src/data.js
+++ src/data.js
@@ -16,13 +16,13 @@ export const quizData = {
   // the questions in the quiz
   questions: [
     {
-      text: 'What are the different ways to declare a JS variable?',
+      text: 'Click the best synonym for constitution:',
       answers: {
-        a: 'constant, let, variable',
-        b: 'var, const, let, function',
-        c: 'var, let, const',
+        a: 'system of laws',
+        b: 'card games',
+        c: 'faraway galaxy',
       },
-      correct: 'c',
+      correct: 'a',
       selected: null,
       links: [
         {
@@ -36,12 +36,12 @@ export const quizData = {
       ],
     },
     {
-      text: 'What does `typeof` do?',
+      text: 'Click the best synonym for resort:',
       answers: {
-        a: 'changes the type of a primitive value',
-        b: 'returns a string describing the type of a value',
-        c: 'determines if a value is primitive',
-        d: 'can tell the difference between arrays and objects',
+        a: 'empty room',
+        b: 'vacation place',
+        c: 'jungle',
+        d: 'tunnel',
       },
       correct: 'b',
       selected: null,
@@ -57,6 +57,177 @@ export const quizData = {
         },
       ],
     },
-    // Add more questions here
+    {
+      text: 'Click the best synonym for sophisticated:',
+      answers: {
+        a: 'strong',
+        b: 'cheap',
+        c: 'experienced',
+        d: 'true',
+      },
+      correct: 'c',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/types#type-typeof',
+        },
+        {
+          text: 'MDN',
+          href:
+            'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+        },
+      ],
+    },
+    {
+        text: 'Click the best synonym for corridor:',
+        answers: {
+          a: 'passage',
+          b: 'symbol',
+          c: 'exit',
+          d: 'rust',
+        },
+        correct: 'a',
+        selected: null,
+        links: [
+          {
+            text: 'javascript.info',
+            href: 'https://javascript.info/types#type-typeof',
+          },
+          {
+            text: 'MDN',
+            href:
+              'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+          },
+        ],
+    },
+    {
+      text: 'Click the best synonym for bid:',
+      answers: {
+        a: 'offer',
+        b: 'stamp',
+        c: 'portray',
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'Click the best synonym for mention:',
+      answers: {
+        a: 'oppose',
+        b: 'create',
+        c: 'create',
+        d: 'refer to',
+      },
+      correct: 'd',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'Click the best synonym for guidance:',
+      answers: {
+        a: 'principal',
+        b: 'advice',
+        c: 'create',
+        d: 'refer to',
+      },
+      correct: 'b',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'Click the best synonym for nervous:',
+      answers: {
+        a: 'principal',
+        b: 'advice',
+        c: 'worried',
+        d: 'refer to',
+      },
+      correct: 'c',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'Click the best synonym for succeed',
+      answers: {
+        a: 'principal',
+        b: 'achieve',
+        c: 'worried',
+        d: 'refer to',
+      },
+      correct: 'b',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'Click the best synonym for vision',
+      answers: {
+        a: 'principal',
+        b: 'sight',
+        c: 'worried',
+        d: 'refer to',
+      },
+      correct: 'b',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    
+    
+
   ],
 };
